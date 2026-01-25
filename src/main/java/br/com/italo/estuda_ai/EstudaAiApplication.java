7391ba2cc7199027cb6eae1fa4294a4505dce897
Italo Santos <italosantoscont@gmail.com>
2026-01-24 22:45:03 -0300

    Main.java

---
+++ src/main/java/br/com/italo/estuda_ai/EstudaAiApplication.java
@@ -0,0 +1,13 @@
+package br.com.italo.estuda_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudaAiApplication.class, args);
+	}
+
+}
