0a51cef0a92c12d9f37ff10448ff96517a9578b1
Italo Santos <italosantoscont@gmail.com>
2026-01-24 22:41:11 -0300

    Criando projeto

---
+++ src/test/java/br/com/italo/estuda_ai/EstudaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.italo.estuda_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
